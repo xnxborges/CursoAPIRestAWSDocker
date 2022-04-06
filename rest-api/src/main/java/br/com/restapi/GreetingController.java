bbdfd03d2a96c389e2edf24b363e633662804ac4
Natan Borges <natan.borges@capgemini.com>
2022-04-06 13:53:33 -0300

    Commit da estrutura do projeto e primeiro endpoint

---
+++ rest-api/src/main/java/br/com/restapi/GreetingController.java
@@ -0,0 +1,21 @@
+package br.com.restapi;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	
+	private static final String template = "Hello, %s!";
+	private static final AtomicLong counter = new AtomicLong();
+	
+	//Endpoint
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue="world") String name)	{
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}	
+}
